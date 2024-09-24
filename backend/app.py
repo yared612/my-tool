9d50e392ed4c3d2d19c4a3db3c3b166ffdbce8f7
yared612 <penguin6241@gmail.com>
2024-09-24 21:20:54 +0800

    fix: 🐛 flask define

--- backend/app.py
+++ backend/app.py
@@ -17,11 +17,7 @@ from utils.auto_calendar import *
 # load dot env setting
 load_dotenv()
 
-app = Flask(
-    app=Flask(
-        __name__, static_folder="static", static_url_path="/static", static_cache_timeout=3600
-    )
-)
+app = Flask(__name__, static_folder="static", static_url_path="/static", static_cache_timeout=3600)
 swagger = Swagger(app)
 app.secret_key = os.urandom(24)
 
